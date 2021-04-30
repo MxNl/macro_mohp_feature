729e0c2f58e77841d459a397ca5abeaf16b6bd17
MxNl <maxnoelscher@posteo.de>
2021-04-30 12:17:55 +0200

    git chaos fixed, works and up to date

--- R/grass_functions.R
+++ R/grass_functions.R
@@ -68,13 +68,13 @@ calculate_mohp_metrics_in_grassdb <-
       # filepaths_reference_raster <- tar_read(filepaths_reference_raster_write)
       studyarea <- tar_read(selected_studyarea)
       streamorder <- 1
-      coastline <- tar_read(coastline_unioned_all)
+      coastline <- tar_read(coastline_watershed)
     }
-
+    
     # Initiate GRASS database
     crs_reference <- st_crs(studyarea)$proj4string
     initiate_grass_db_parallel(studyarea, streamorder, crs_reference)
-
+    
     # Get river network from PostGIS databse
     lines <- 
       st_read(
@@ -92,7 +92,7 @@ calculate_mohp_metrics_in_grassdb <-
       coastline %>%
       pull(feature_id) %>%
       unique()
-
+    
     lines_to_remove_from_rivers <-
       coastline %>%
       filter(type == "watershed") %>% 
@@ -129,7 +129,7 @@ calculate_mohp_metrics_in_grassdb <-
         has_inland_waters,
         streamorder,
         n_studyareas
-        )
+      )
   }
 
 calculate_mohp_per_polygon <- 
@@ -146,13 +146,13 @@ calculate_mohp_per_polygon <-
               n = as.character(b_box["ymax"]), s = as.character(b_box["ymin"]),
               e = as.character(b_box["xmax"]), w = as.character(b_box["xmin"]),
               res = as.character(CELLSIZE))
-
+    
     studyarea %>%
       grass_calculations(
         lines_to_remove_from_catchments,
         lines_to_remove_from_rivers,
         has_inland_waters)
-
+    
     region_name <- studyarea %>% pull(region_name)
     
     FEATURE_NAMES %>%
@@ -194,7 +194,7 @@ grass_calculations <-
            lines_to_remove_from_catchments, 
            lines_to_remove_from_rivers, 
            has_inland_waters) {
-
+    
     # feature_layernames <- feature_layernames %>% chuck(1)
     # studyarea <- studyarea %>% chuck(1)
     
@@ -208,16 +208,19 @@ grass_calculations <-
     print("r.mask")
     
     execGRASS("r.null",
-              map = "river_network_value_raster_thin",
+              map = "river_network_raster",
               setnull = as.character(lines_to_remove_from_rivers))
     print("r.null")
+    
     execGRASS("r.grow.distance",
-              input = "river_network_value_raster_thin",
+              input = "river_network_raster",
               distance = "river_network_distance_raster", 
               value = "river_network_value_raster", 
               flags = c("overwrite", "m"))
     print("r.grow.distance")
     
+    
+    # Generate thiessen catchments
     execGRASS("r.null",
               map = "river_network_value_raster",
               setnull = as.character(lines_to_remove_from_catchments))
@@ -228,8 +231,14 @@ grass_calculations <-
               type = "area", 
               flags = c("overwrite"))
     
-    execGRASS("v.to.lines",
+    execGRASS("v.clean",
               input = "thiessen_catchments",
+              output = "thiessen_catchments_clean",
+              tool = "rmdangle",
+              flags = c("overwrite"))
+    
+    execGRASS("v.to.lines",
+              input = "thiessen_catchments_clean",
               output = "thiessen_catchments_outline", 
               flags = c("overwrite"))
     
@@ -244,7 +253,7 @@ grass_calculations <-
               map = "thiessen_catchments_outline_cat", 
               table = "thiessen_catchments_outline_cat", 
               flags = c("o"))
-
+    
     execGRASS("v.to.rast", 
               input = "thiessen_catchments_outline_cat", 
               output = "thiessen_catchments_lines_raster", 
@@ -262,11 +271,36 @@ grass_calculations <-
               iterations = 200,
               flags = c("overwrite"))
     
-    execGRASS("r.grow.distance",
-              input = "thiessen_catchments_lines_raster_thin",
-              distance = "thiessen_catchments_distance_raster",
-              flags = c("overwrite", "m"))
-    print("r.grow.distance")
+    
+    # Calculate distance from watershed/catchment boundary
+    use_sp()
+    max_distance <- 
+      readRAST("river_network_distance_raster") %>%
+      raster() %>% 
+      raster::maxValue()
+    
+    execGRASS("r.mapcalc",
+              expression = str_glue("friction = if(!isnull(river_network_raster), {max_distance}, 0)"),
+              flags = c("overwrite"))
+    
+    execGRASS("r.walk",
+              elevation = "river_network_distance_raster",
+              friction = "friction",
+              output = "thiessen_catchments_distance_raster",
+              start_raster = "thiessen_catchments_lines_raster_thin",
+              walk_coeff = "1,0,0,0",
+              lambda = 1000,
+              flags = c("overwrite"))
+    execGRASS("r.mapcalc",
+              expression = "thiessen_catchments_distance_raster = if(!isnull(river_network_raster), 0, thiessen_catchments_distance_raster)",
+              flags = c("overwrite"))
+    #TODO Remove after bracket splits are implemented
+    execGRASS("r.mapcalc",
+              expression = str_glue("thiessen_catchments_distance_raster = if(thiessen_catchments_distance_raster > {max_distance}*1.3, {max_distance}, thiessen_catchments_distance_raster)"),
+              flags = c("overwrite"))
+    print("thiessen_catchments_distance_raster")
+    
+    # Final MOHP calculation
     execGRASS("r.mapcalc",
               expression = glue::glue("{FEATURE_NAMES[1]} = (river_network_distance_raster + thiessen_catchments_distance_raster)"),
               flags = c("overwrite"))
@@ -296,7 +330,7 @@ grass_rasterize_and_clean_rivers <-
               output = "river_network_raster", 
               use = "attr",
               attribute_column = "feature_id",
-              flags = c("overwrite"),
+              flags = c("overwrite", "d"),
               memory = GRASS_MAX_MEMORY)
     print("v.to.rast")
     
@@ -307,11 +341,11 @@ grass_rasterize_and_clean_rivers <-
     execGRASS("r.null",
               map = "river_network_raster")
     
-    execGRASS("r.thin",
-              input = "river_network_raster",
-              output = "river_network_value_raster_thin",
-              flags = c("overwrite"))
-    print("r.thin")
+    # execGRASS("r.thin",
+    #           input = "river_network_raster",
+    #           output = "river_network_value_raster_thin",
+    #           flags = c("overwrite"))
+    # print("r.thin")
   }
 
 add_inland_waters_to_rivers_raster <- 
@@ -345,8 +379,8 @@ add_inland_waters_to_rivers_raster <-
     
     if(has_inland_waters) {
       execGRASS("r.patch",
-                input = c("river_network_value_raster_thin", "inland_waters_raster"),
-                output = "river_network_value_raster_thin",
+                input = c("river_network_raster", "inland_waters_raster"),
+                output = "river_network_raster",
                 flag = c("overwrite"))
     }
     
