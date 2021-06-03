5d4b945be3ee3e731d6067f2c9548e7612b40c2c
michael <michael.mutz@posteo.de>
2021-06-04 01:28:20 +0200

    ???

--- main.R
+++ main.R
@@ -1,30 +1,13 @@
 library(tidyverse)
+library(rgrass7)
 library(sf)
-library(furrr)
 message('Hi!')
 message(getwd())
 system('whoami')
-system('ls -l')
-
 writeLines('Yo!', 'docker-mount/test.txt')
 
 writeLines('Yo!', 'docker-mount/yeehaw.txt')
-
-plan(multisession, workers = 8)
-
-partitions <- 5
-
-df <- tibble(
-  a = 1:30e6,
-  b = rnorm(length(a))
-)
-
-df %>% 
-  mutate(partition_id = row_number() %% 5) %>% 
-  split(.[["partition_id"]]) %>% 
-  future_map_dfr(~{
-    .x %>% 
-      rowwise() %>% 
-      mutate(b = list(tibble(a = b * 2))) %>%
-      ungroup()
-  })
+link = link2GI::findGRASS()
+print(link)
+#system2("find", "/usr ! -readable -prune -o -type f -executable -iname 'grass??' -print")
+#initGRASS(gisBase = "/usr/share")
