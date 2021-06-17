333b4fc74f083a5271e30c9c2a814f90d7e2b7a1
MxNl <maxnoelscher@posteo.de>
2021-06-17 13:12:29 +0200

    only calculate and output intersecting studyarea polygons

--- R/grass_functions.R
+++ R/grass_functions.R
@@ -45,7 +45,7 @@ calculate_mohp_metrics_in_grassdb <-
       # reference_raster <- tar_read(reference_raster)
       # filepaths_reference_raster <- tar_read(filepaths_reference_raster_write)
       studyarea <- tar_read(selected_studyarea)
-      streamorder <- 1
+      streamorder <- 3
       coastline <- tar_read(coastline_watershed)
     }
     
@@ -99,6 +99,7 @@ calculate_mohp_metrics_in_grassdb <-
     studyarea <- 
       studyarea %>% 
       st_cast("POLYGON") %>% 
+      filter_intersecting_features(lines) %>% 
       st_transform(crs_reference) %>%
       rowwise() %>% 
       group_split()
